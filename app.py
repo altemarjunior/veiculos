786395e3c638d27c17cf110db31a2c9b308a41c4
ALTEMAR JÚNIOR <altemarjrengenharia@gmail.com>
2024-11-13 09:52:22 -0400

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ import pytz
 def carregar_checkins():
     return pd.read_excel('dados_checkin_checkout.xlsx')
 
-# Função para atualizar um check-out no arquivo
+# Função para registrar o check-in no arquivo
+def registrar_checkin(checkin_novo):
+    df = carregar_checkins()
+    df = df.append(checkin_novo, ignore_index=True)
+    df.to_excel('dados_checkin_checkout.xlsx', index=False)
+
+# Função para registrar o check-out no arquivo
 def atualizar_checkout(checkin_atualizado):
     df = carregar_checkins()
     for index, row in df.iterrows():
@@ -18,43 +24,45 @@ def atualizar_checkout(checkin_atualizado):
             df.at[index, 'data_hora_checkout'] = checkin_atualizado['data_hora_checkout']
     df.to_excel('dados_checkin_checkout.xlsx', index=False)
 
-# Função para exibir o formulário de check-out de veículo
-def exibir_formulario_checkout(usuario):
-    st.title("CONTROLE DE VEÍCULOS - CHECK-OUT")
+# Função para exibir o formulário de check-in de veículo
+def exibir_formulario_checkin(usuario):
+    st.title("CONTROLE DE VEÍCULOS - CHECK-IN")
     
     df = carregar_checkins()
     checkins_abertos = df[(df['usuario'] == usuario) & (df['km_final'].isna())]
 
     if not checkins_abertos.empty:
-        carro = st.selectbox("ESCOLHA O VEÍCULO PARA CHECK-OUT", checkins_abertos['carro'].unique())
+        carro = st.selectbox("ESCOLHA O VEÍCULO PARA CHECK-IN", checkins_abertos['carro'].unique())
         
         # Obter o km_inicial do veículo selecionado
         km_inicial = checkins_abertos[checkins_abertos['carro'] == carro]['km_inicial'].iloc[0]
         
-        # Exibir o campo para o KM final, com um valor mínimo sendo o km_inicial
-        km_final = st.number_input("KM FINAL", min_value=km_inicial, value=km_inicial)
+        # Exibir o campo para o KM de checkout, com um valor mínimo sendo o km_inicial
+        km_checkout = st.number_input("KM DE CHECKOUT", min_value=km_inicial, value=km_inicial)
 
         # Definir o fuso horário do Amazonas (UTC-4)
         fuso_amazonas = pytz.timezone('America/Manaus')
         
         # Obter a data e hora atual (horário local) ajustado para UTC-4
-        data_hora_checkout = datetime.datetime.now() - datetime.timedelta(hours=4)
+        data_hora_checkin = datetime.datetime.now() - datetime.timedelta(hours=4)
 
-        if st.button("REGISTRAR CHECK-OUT"):
-            # Verificar se o km_final não é menor que o km_inicial
-            if km_final >= km_inicial:
-                checkin_atualizado = {
+        if st.button("REGISTRAR CHECK-IN"):
+            # Verificar se o km_checkout não é menor que o km_inicial
+            if km_checkout >= km_inicial:
+                checkin_novo = {
                     "carro": carro,
-                    "km_final": km_final,
+                    "km_inicial": km_checkout,
                     "usuario": usuario,
-                    "data_hora_checkout": data_hora_checkout,
+                    "data_hora_checkin": data_hora_checkin,
+                    "km_final": None,
+                    "data_hora_checkout": None
                 }
-                atualizar_checkout(checkin_atualizado)
-                st.success(f"CHECK-OUT REALIZADO COM SUCESSO PARA O CARRO {carro}!")
+                registrar_checkin(checkin_novo)
+                st.success(f"CHECK-IN REALIZADO COM SUCESSO PARA O CARRO {carro}!")
             else:
-                st.error("O KM FINAL não pode ser menor que o KM INICIAL.")
+                st.error("O KM DE CHECKOUT não pode ser menor que o KM INICIAL.")
     else:
-        st.info("Nenhum check-in pendente para este usuário.")
+        st.info("Nenhum check-out pendente para este usuário.")
 
 # Função principal
 def app():
@@ -62,7 +70,7 @@ def app():
     usuario = st.sidebar.text_input("Digite seu nome de usuário:")
 
     if usuario:
-        exibir_formulario_checkout(usuario)
+        exibir_formulario_checkin(usuario)
 
 if __name__ == "__main__":
     app()
